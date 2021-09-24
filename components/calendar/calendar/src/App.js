1de633bfc453e0658940aae347d07d9f2b4fc3e7
Alexplus77 <alexplus77@mail.ru>
2021-09-24 12:47:26 +0200

    calendar.js ошибки в разметке

---
+++ components/calendar/calendar/src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Calendar from "./calendar";
+import './css/main.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Calendar/>
+    </React.Fragment>
+
+
+  );
+}
+
+export default App;
